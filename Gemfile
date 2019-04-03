608ca65ab2530311adf16d0cf41f5da90f9bd3e6
okazuya1 <tttt123987t@yahoo.co.jp>
2019-04-03 18:53:58 +0900

    ログイン
    ログイン

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'rb-readline'
 gem "bcrypt","~>3.1.7"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem "rails_autolink"
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,18 +19,18 @@ class Admin::UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
 
-  	if @user.save
-  	  redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」"を登録しました。
-  	else
-  	  render :new
-  	end
+    if @user.save
+       redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+       render :new
+    end
   end
 
   def uptate
     @user = User.find(params[:id])
 
     if @user.uptate(user_params)
-      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」"を更新しました。
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
     else
       render :new
     end
@@ -39,12 +39,16 @@ class Admin::UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_url, notice: "ユーザー「#{task.name}｣を削除しました"
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}｣を削除しました"
   end
 
-  praivate
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_path unless current_user.admin?
   end
 end
